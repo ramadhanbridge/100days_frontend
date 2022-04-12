f35dc7f69fe89f790ac0c9b3dffaeb6d6e5e09b5
ramadhanbridge <nyiringondoramadan@gmail.com>
2022-04-12 10:22:56 +0200

    project 5

--- projects/50 by Brad/project 5/script.js
+++ projects/50 by Brad/project 5/script.js
@@ -2,18 +2,31 @@ let percent = document.querySelector('.percent')
 let bodyfilter = document.querySelector('.bodyfilter')
 let count = 0;
 let runsession = setInterval(windostart, 100 )
-
-
+let test = 0;
 
 function windostart() {
-   
-    count == 100 ? clearInterval(runsession) : count++ 
-    percent.textContent = `${count}%`
-    percent.style.opacity = scale(count,1,100,1,0)
-    bodyfilter.style.backdropFilter = `blur(${scale(count,1,100,70,0)}px)`
+    //  document.readyState == "complete"
+    if(count == 100 || document.readyState == "complete" ){
+        cleanUp()
+    }else{
+        count++ 
+        percent.textContent = `${count}%`
+        percent.style.opacity = scale(count,1,100,1,0)
+        bodyfilter.style.backdropFilter = `blur(${scale(count,1,100,70,0)}px)`
+    }
+    
 }
 
-
+function cleanUp(){
+    setTimeout(() => {
+        clearInterval(runsession) 
+        percent.textContent = `100%`
+        percent.style.opacity = '0'
+        bodyfilter.style.backdropFilter = `blur(0px)`
+    }, 1000);
+    
+}
 function scale(number, inMin, inMax, outMin, outMax) {
     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
+
